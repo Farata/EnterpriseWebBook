85221347a8d8029228122f251f39592825d1a865
Yakov Fain <yakovfain@gmail.com>
2012-11-20 14:55:38 -0500

    Added a section on Prototypal Inheritance

--- 1_Desktop/02_JavaScript/codesamples/functions_objects/WhoIsYourDaddy.js
+++ 1_Desktop/02_JavaScript/codesamples/functions_objects/WhoIsYourDaddy.js
@@ -10,7 +10,20 @@ function Employee(name, title){
 	this.title=title;
 }
 
+// All instances of Employee will extend Person
 Employee.prototype=new Person();
+
 var emp=new Employee("Mary", "Specialist");
 
-console.log("Created Employee "+ emp);
+console.log(emp);
+
+//Create an instance of Person and add property dependents 
+var p=new Person();
+p.dependents=1;
+
+
+var emp2=new Employee("Joe", "Father");
+//This employee will have property dependents
+emp2.__proto__=p;
+
+console.log("The number of Employee's dependents "+ emp2.dependents);
