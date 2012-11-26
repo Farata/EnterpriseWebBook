87a20f3400bd26bb9e9039a381aad2573bda581f
Yakov Fain <yakovfain@gmail.com>
2012-11-26 18:23:11 -0500

    Added a piece to the JS, Browser and DOM section

--- 1_Desktop/02_JavaScript/codesamples/functions_objects/wt2.js
+++
@@ -1,14 +0,0 @@
-function Person(lname, fname, age){
-         this.lastName=lname;
-         this.firstName=fname;
-         this.age=age;
-      
-         this.marryMe=function(person){
-         	console.log("Will you marry me " + person.lastName);
-         	
-         };
-
- };
- 
-var p1= new Person("Fain", "Yakov");var p2= new Person("Roberts", "Julia");
-p1.marryMe(p2);
