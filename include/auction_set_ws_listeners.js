88a433af50687ea7f533e238999fe05aab1d661a
Viktor Gamov <gamussa@gmail.com>
2012-12-20 03:13:57 -0500

    Begin work on: - loading modules on demand - setting listeners - sending messages

---
+++ include/auction_set_ws_listeners.js
@@ -0,0 +1,19 @@
+ if(window.WebSocket) {
+   webSocket = new WebSocket("ws://localhost:8080/child-auction/auction");
+   webSocket.onopen = function() {
+     console.log("connection open...");         //<1>
+     getAuctionsList();
+   };
+   webSocket.onclose = function(closeEvent) {
+     console.log("close code " + closeEvent.code);
+   };
+   webSocket.onmessage = function(messageEvent) {
+     console.log("data from server: " + messageEvent.data);
+     if(typeof messageEvent.data === "string") {
+       handleMessage(messageEvent.data);
+     }
+   };
+   return webSocket.onerror = function() {
+     console.log("websocket error");
+   };
+ }
---
+++ include/loading_modules_on_demand.js
@@ -0,0 +1,31 @@
+require([], function() {    // <1>
+  return(function() {
+    var lazyLoadingEventHandlerFactory, wayToGiveHandleClick, wayToGiveModule, way_to_give;
+    way_to_give = document.getElementById("way-to-give");
+
+    wayToGiveModule = null;     // <2>
+
+    lazyLoadingEventHandlerFactory = function(module, modulePath) {
+      var clickEventHandler;
+      clickEventHandler = function(event) {
+        console.log(event.target);
+        if(module === "loading") {    // <3>
+          return;
+        }
+        if(module !== null) {
+          return module.startAuction();      // <4>
+        } else {
+          module = "loading";                // <5>
+          return require([modulePath], function(moduleObject) { // <6>
+            module = new moduleObject();
+            return module.render();                             // <7>
+          });
+        }
+      };
+      return clickEventHandler;
+    };
+    wayToGiveHandleClick = lazyLoadingEventHandlerFactory(wayToGiveModule, "modules/way-to-give");
+
+    way_to_give.addEventListener("click", wayToGiveHandleClick, false);   // <8>
+  })();
+});
