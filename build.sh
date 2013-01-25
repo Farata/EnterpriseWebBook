0d89456b1cebc8a1cb2518a98e6228cfec76df91
Viktor Gamov <gamussa@gmail.com>
2013-01-25 10:20:13 -0500

    reverted build script for previous template theme

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #generate book, toclivel attribute is responsible for deep of table of contents
-##asciidoc -d book -b html5 -a icons -a iconsdir=`brew --prefix asciidoc`/etc/asciidoc/images/icons -a theme=flask -a data-uri -a toc2 -a toclevels=3 --out-file book.html site.asciidoc
-asciidoc -d book -b bootstrap-docs -a icons -a iconsdir=`brew --prefix asciidoc`/etc/asciidoc/images/icons -a data-uri -a toc2 -a toclevels=2 --out-file book.html site.asciidoc
+asciidoc -d book -b html5 -a icons -a iconsdir=`brew --prefix asciidoc`/etc/asciidoc/images/icons -a theme=flask -a data-uri -a toc2 -a toclevels=3 --out-file book.html site.asciidoc
+#asciidoc -d book -b bootstrap-docs -a icons -a iconsdir=`brew --prefix asciidoc`/etc/asciidoc/images/icons -a data-uri -a toc2 -a toclevels=2 --out-file book.html site.asciidoc
 
 #ubuntu version version of build command
 #asciidoc -d book -b html5 -a icons -a iconsdir=/etc/asciidoc/images/icons -a theme=flask -a data-uri -a toc2 -a toclevels=3 book.asciidoc
