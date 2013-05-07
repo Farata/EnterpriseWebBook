6afab41d6ada36238156f97deda8ae0efcd0be80
Viktor Gamov <gamussa@gmail.com>
2013-05-07 01:20:36 -0400

    added grunt with jshint and watch tasks section

---
+++ include/jshint_example.js
@@ -0,0 +1,9 @@
+var bonds = [                   // <1>
+            'Sean Connery',
+            'George Lazenby',
+            'Roger Moore',
+            'Timothy Dalton',
+            'Pierce Brosnan',
+            'Daniel Craig',     // <2>
+            //'Unknow yet actor' 
+        ]                       // <3>
