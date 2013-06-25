4b001566e29b3bcb283bd3c812c39e851ebef184
Viktor Gamov <gamussa@gmail.com>
2013-06-25 03:00:19 -0400

    - fixes from email - AMD section, examples - Chapter summary

---
+++ include/ch7_define_wayToGive.js
@@ -0,0 +1,19 @@
+define(["newContentLoader"], function(contentLoader) { // <1>
+    var wayToGive;
+
+    console.log("way-to-give module is loaded");
+    wayToGive = function() {
+        return {
+            render: function() {
+                contentLoader.getNewContent("way-to-give", // <2>
+                                            "way-to-give-container", 
+                                            "assets/html-includes/way-to-give.html");
+                return console.log("way-to-give module is rendered");
+            },
+            init: function() {
+                return console.log("way-to-give init");
+            }
+        };
+    };
+    return wayToGive;   // <3>
+});
