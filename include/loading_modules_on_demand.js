49e55709589cfe2160d9b9ea1ad4449bc07519de
Viktor Gamov <gamussa@gmail.com>
2013-07-24 00:19:40 -0400

    jshinted JavaScript code snippets

--- include/loading_modules_on_demand.js
+++ include/loading_modules_on_demand.js
@@ -1,31 +1,32 @@
-require([], function() {    // <1>
-  return(function() {
+require([], function() { // <1>
+  'use strict';
+  return (function() {
     var lazyLoadingEventHandlerFactory, wayToGiveHandleClick, wayToGiveModule, way_to_give;
-    way_to_give = document.getElementById("way-to-give");
+    way_to_give = document.getElementById('way-to-give');
 
-    wayToGiveModule = null;     // <2>
+    wayToGiveModule = null; // <2>
 
     lazyLoadingEventHandlerFactory = function(module, modulePath) {
       var clickEventHandler;
       clickEventHandler = function(event) {
         console.log(event.target);
-        if(module === "loading") {    // <3>
+        if (module === 'loading') { // <3>
           return;
         }
-        if(module !== null) {
-          return module.startAuction();      // <4>
+        if (module !== null) {
+          return module.startAuction(); // <4>
         } else {
-          module = "loading";                // <5>
-          return require([modulePath], function(moduleObject) { // <6>
-            module = new moduleObject();
-            return module.render();                             // <7>
+          module = 'loading'; // <5>
+          return require([modulePath], function(ModuleObject) { // <6>
+            module = new ModuleObject();
+            return module.render(); // <7>
           });
         }
       };
       return clickEventHandler;
     };
-    wayToGiveHandleClick = lazyLoadingEventHandlerFactory(wayToGiveModule, "modules/way-to-give");
+    wayToGiveHandleClick = lazyLoadingEventHandlerFactory(wayToGiveModule, 'modules/way-to-give');
 
-    way_to_give.addEventListener("click", wayToGiveHandleClick, false);   // <8>
+    way_to_give.addEventListener('click', wayToGiveHandleClick, false); // <8>
   })();
 });
