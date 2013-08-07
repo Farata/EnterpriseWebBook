a3ec9ea8d21187b11e1564d18156cc12f9e2b19f
Viktor Gamov <gamussa@gmail.com>
2013-08-07 00:33:29 -0400

    finished commonjs section

--- include/ch7_iife_loginModule.js
+++ include/ch7_iife_loginModule.js
@@ -1,15 +1,23 @@
-var loginModule = (function() {
-    'use strict';
+var loginModule = (function() {     // <1>
+    "use strict";
 
     var module = {};
+    var privateVariable = 42;
+
+    var privateLogin = function(userNameValue, userPasswordValue) {     // <2>
+        if (userNameValue === "admin" && userPasswordValue === "secret") {
+            return privateVariable;
+        }
+    };
 
     module.myConstant = 1984;
     module.login = function(userNameValue, userPasswordValue) {
-        console.log('login implementation omitted');
+        privateLogin(userNameValue, userPasswordValue);
+        console.log("login implementation omitted");
     };
 
     module.logout = function() {
-        console.log('logout implementation omitted');
+        console.log("logout implementation omitted");
     };
 
     return module;
